aebbece1fce8f0bc37378a5777d88a43474272ea
hazim1093 <hazim_malik@hotmail.com>
2017-02-22 19:01:20 +0500

    Update start script

--- 6.9/start.sh
+++ 6.9/start.sh
@@ -3,16 +3,18 @@ set -e
 
 # Add node as command if needed
 if [ "${COMMAND:0:1}" = '-' ]; then
-	set -- stakater "$COMMAND"
+	set -- node "$COMMAND"
 fi
 
 # Drop root privileges if we are running node
 # allow the container to be started with `--user`
-if [ "$COMMAND" = 'node' -a "$(id -u)" = '0' ]; then
+if [ "$COMMAND" = "node"* -a "$(id -u)" = '0' ]; 
+then
 	set -- su-exec stakater "$COMMAND"
-fi
-
+else
 # As argument is not related to node,
 # then assume that user wants to run his own process,
 # for example a `bash` shell to explore this image
 exec "$COMMAND"
+fi
+
